67541d065c645eedc9dc40d73dfbee980f6b85d7
Richard Neumann <mail@richard-neumann.de>
2021-01-23 12:15:53 +0100

    Refactored start, stop, open and close.

--- drivers/hid/amd-sfh-hid/amd-sfh-hid-ll-drv.c
+++ drivers/hid/amd-sfh-hid/amd-sfh-hid-ll-drv.c
@@ -80,7 +80,6 @@ static int amd_sfh_hid_ll_start(struct hid_device *hid)
 	if (!hid_data->cpu_addr)
 		return -EIO;
 
-	schedule_delayed_work(&hid_data->work, hid_data->interval);
 	return 0;
 }
 
@@ -95,7 +94,6 @@ static void amd_sfh_hid_ll_stop(struct hid_device *hid)
 	struct amd_sfh_hid_data *hid_data = hid->driver_data;
 
 	hid_err(hid, "STOPPING");
-	cancel_delayed_work_sync(&hid_data->work);
 	dma_free_coherent(&hid_data->pci_dev->dev, AMD_SFH_HID_DMA_SIZE,
 			  hid_data->cpu_addr, hid_data->dma_handle);
 	hid_data->cpu_addr = NULL;
@@ -114,6 +112,7 @@ static int amd_sfh_hid_ll_open(struct hid_device *hid)
 	hid_err(hid, "OPENING");
 	amd_sfh_start_sensor(hid_data->pci_dev, hid_data->sensor_idx,
 			     hid_data->dma_handle, hid_data->interval);
+	schedule_delayed_work(&hid_data->work, hid_data->interval);
 	return 0;
 }
 
